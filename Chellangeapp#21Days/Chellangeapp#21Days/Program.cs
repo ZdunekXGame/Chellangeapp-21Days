54bc6dca49f0bbb9f95e433e16fe09b9e719f704
Sebastian Zduńczyk <zdunekxgaming@gmail.com>
2023-03-01 22:28:43 +0100

    Zadanie Domowe Dzień 4

--- Chellangeapp#21Days/Chellangeapp#21Days/Program.cs
+++ Chellangeapp#21Days/Chellangeapp#21Days/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var Name = ("Sebastian");
+var Sex = ("Men");
+var NumberAge = 22;
+
+if (NumberAge < 22)
+{
+    if (Name == "Sebastian")
+    {
+        Console.WriteLine("Mam na imię Sebastian i mam 22 lata");
+    }
+}
+else
+{
+    if (Name == "Sebastian");
+}
+{
+    if (NumberAge == 22) ;
+}
+{
+    if (Sex == "Men") ;
+}
+{
+    Console.WriteLine("Mam na imię Sebastian mam 22 lata i jestem mężczyzną");
+}
