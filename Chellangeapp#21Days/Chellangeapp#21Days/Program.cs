9102d6d5f0936929487683f521ffe61779e92a86
Sebastian Zduńczyk <zdunekxgaming@gmail.com>
2023-03-09 23:37:29 +0100

    Dzień 4

--- Chellangeapp#21Days/Chellangeapp#21Days/Program.cs
+++ Chellangeapp#21Days/Chellangeapp#21Days/Program.cs
@@ -7,7 +7,7 @@ int age = 22;
 
 if (man == true)
 {
-    if (age == 22 && name == "Sebastian")
+    if (age >= 23 && name == "Sebastian")
     {
         Console.WriteLine("Sebastian, age 22");
     }
@@ -22,9 +22,16 @@ if (man == true)
         {
             Console.WriteLine("Underage male");
         }
-        else if (man == true)
+        else if (man == false)
         {
             Console.WriteLine("A man is born");
         }
     }
+}
+ 
+{
+    if (age == 22 && name == "Sebastian" && man == true)
+    {
+        Console.WriteLine("It's me, a 22-years-old man named Sebastian");
+            }
 }
